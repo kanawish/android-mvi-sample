023d81aa8a5d032656367b9d943c50455b3c5823
Etienne Caron <etienne.caron@shopify.com>
2019-01-04 11:15:38 -0500

    **[Starting Point v2]**

--- app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskActivity.kt
+++ app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskActivity.kt
@@ -2,8 +2,4 @@ package com.kanawish.sample.mvi.view.addedittask
 
 import android.support.v7.app.AppCompatActivity
 
-/**
- * Created on 2017-08-10.
- */
-class AddEditTaskActivity : AppCompatActivity() {
-}
+class AddEditTaskActivity : AppCompatActivity()
