9a2b6eb803d78be1797b9b209dff352d1d1888a5
Etienne Caron <etienne.caron@shopify.com>
2019-02-12 17:12:11 -0500

    **Lesson: Setting up an Android MVI project**
    We've covered the basic theory behind MVI. We now want to apply it in practice. In this lesson, let's set up our Android MVI project. We'll sketch the overall structure of our app. Then we'll go over what libraries we will be using, and why.

--- app/src/main/java/com/kanawish/sample/mvi/TasksApp.kt
+++ app/src/main/java/com/kanawish/sample/mvi/TasksApp.kt
@@ -1,5 +1,49 @@
 package com.kanawish.sample.mvi
 
 import android.app.Application
+import com.kanawish.sample.mvi.di.ToothpickActivityLifecycleCallbacks
+import timber.log.Timber
+import toothpick.Scope
+import toothpick.Toothpick
+import toothpick.smoothie.module.SmoothieApplicationModule
 
-class TasksApp : Application()
+/**
+ * TaskApp sets up Application level concerns:
+ * - Timber for better log management.
+ * - Toothpick for DI.
+ *
+ * DI scopes are:
+ * Application[ Activity [ Fragment ] ]
+ *
+ * Activity scope lifecycle: from `onActivityCreated()` to `onActivityDestroyed()`
+ * Fragment scope lifecycle: from `onFragmentPreAttached()` to `onFragmentDetached()`
+ */
+class TasksApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Logger init
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        Timber.i("%s %d", BuildConfig.VERSION_NAME, BuildConfig.VERSION_CODE)
+
+        // DI Root Scope init
+        Toothpick.inject(this, openApplicationScope(this))
+        registerActivityLifecycleCallbacks(ToothpickActivityLifecycleCallbacks())
+    }
+
+    /**
+     * A very basic Application scope.
+     */
+    private fun openApplicationScope(app: Application): Scope {
+        return Toothpick.openScope(app).apply {
+            installModules(
+                    SmoothieApplicationModule(app)
+            )
+        }
+    }
+
+}
