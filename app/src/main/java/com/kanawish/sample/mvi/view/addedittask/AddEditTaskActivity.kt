38a9a6cb811b63c4db5a206f62cee352f9a3be4f
Etienne Caron <etienne.caron@shopify.com>
2019-02-18 11:20:11 -0500

    **Lesson: View and View Events** Next, we focus on the V of MVI, Views. Views are the source of user interaction events. We name these *view events*. In this lesson, we create our first ViewEvent type with the help of Kotlin sealed classes. We also see how to use the RxBinding library to create Observables of ViewEvent.

--- app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskActivity.kt
+++ app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskActivity.kt
@@ -2,18 +2,38 @@ package com.kanawish.sample.mvi.view.addedittask
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.jakewharton.rxbinding2.support.v7.widget.itemClicks
+import com.jakewharton.rxbinding2.view.clicks
 import com.kanawish.sample.mvi.R
 import com.kanawish.sample.mvi.util.replaceFragmentInActivity
 import com.kanawish.sample.mvi.util.setupActionBar
+import com.kanawish.sample.mvi.view.EventObservable
+import io.reactivex.Observable
+import kotlinx.android.synthetic.main.addtask_act.fab_edit_task_done
+import kotlinx.android.synthetic.main.addtask_act.toolbar
 
 /**
  * Activity houses the Toolbar, a FAB and the fragment for adding/editing tasks.
  */
-class AddEditTaskActivity : AppCompatActivity() {
+class AddEditTaskActivity : AppCompatActivity(),
+    EventObservable<AddEditTaskViewEvent> {
+
+    override fun events(): Observable<AddEditTaskViewEvent> {
+        return Observable.merge(
+                toolbar.itemClicks()
+                    .filter { it.itemId == R.id.menu_delete }
+                    .map { AddEditTaskViewEvent.DeleteTaskClick },
+                fab_edit_task_done.clicks().map { AddEditTaskViewEvent.SaveTaskClick }
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.addtask_act)
+
+        fab_edit_task_done?.apply {
+            setImageResource(R.drawable.ic_done)
+        }
 
         // Set up the toolbar.
         setupActionBar(R.id.toolbar) {
