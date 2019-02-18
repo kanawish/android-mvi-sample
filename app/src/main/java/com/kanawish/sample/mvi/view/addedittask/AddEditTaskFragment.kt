da03dc31cf6c8191e4396050b97f8cd583f0f7bd
Etienne Caron <etienne.caron@shopify.com>
2019-02-18 13:58:52 -0500

    **Lesson: ViewEvents -> Intents** We are now ready to build task editor Intents from our editor ViewEvents. In this lesson we implement `AddEditTaskIntentFactory` and use it to connect our editor View to our editor Model.

--- app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/kanawish/sample/mvi/view/addedittask/AddEditTaskFragment.kt
@@ -9,18 +9,27 @@ import android.view.View
 import android.view.ViewGroup
 import com.jakewharton.rxbinding2.widget.textChanges
 import com.kanawish.sample.mvi.R
+import com.kanawish.sample.mvi.intent.AddEditTaskIntentFactory
 import com.kanawish.sample.mvi.view.EventObservable
 import com.kanawish.sample.mvi.view.addedittask.AddEditTaskViewEvent.DescriptionChange
 import com.kanawish.sample.mvi.view.addedittask.AddEditTaskViewEvent.TitleChange
 import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.plusAssign
 import kotlinx.android.synthetic.main.addtask_frag.add_task_description
 import kotlinx.android.synthetic.main.addtask_frag.add_task_title
+import javax.inject.Inject
 
 /**
  * Fragment for adding/editing tasks.
  */
 class AddEditTaskFragment : Fragment(),
     EventObservable<AddEditTaskViewEvent> {
+
+    @Inject lateinit var intentFactory: AddEditTaskIntentFactory
+
+    private val disposables = CompositeDisposable()
+
     override fun events(): Observable<AddEditTaskViewEvent> {
         return Observable.merge(
                 add_task_title.textChanges().map { TitleChange(it.toString()) },
@@ -35,6 +44,16 @@ class AddEditTaskFragment : Fragment(),
 
     override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater) {
         inflater.inflate(R.menu.addtask_fragment_menu, menu)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        disposables += events().subscribe(intentFactory::process)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        disposables.clear()
     }
 
 }
