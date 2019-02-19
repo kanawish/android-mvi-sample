934cdfefc60dc1be82261e5e340e8c9a3bea456f
Etienne Caron <etienne.caron@shopify.com>
2019-02-19 17:52:41 -0500

    **Lesson: Defining Model and Model State**
    We'll be building a TODO app. So we're looking at a connected list-detail type app, with basic create, update and delete functionality. In this lesson, we'll build our Model State data classes and the Model interface.

---
+++ app/src/main/java/com/kanawish/sample/mvi/model/Model.kt
@@ -0,0 +1,23 @@
+package com.kanawish.sample.mvi.model
+
+import io.reactivex.Observable
+
+interface Model<S> {
+    /**
+     * Model will receive intents to be processed via this function.
+     *
+     * ModelState is immutable. Processed intents will work much like `copy()`
+     * and create a new (modified) modelState from an old one.
+     */
+    fun process(intent:Intent<S>)
+
+    /**
+     * Observable stream of changes to ModelState
+     *
+     * Every time a modelState is replaced by a new one, this observable will
+     * fire.
+     *
+     * This is what views will subscribe to.
+     */
+    fun modelState(): Observable<S>
+}
---
+++ app/src/main/java/com/kanawish/sample/mvi/model/TasksState.kt
@@ -0,0 +1,110 @@
+package com.kanawish.sample.mvi.model
+
+import java.util.*
+
+fun main() {
+    val task = Task(title = "Milk", completed = false)
+    // Want to check off milk from the list?
+    val updatedTask = task.copy(completed = true)
+    // That's all there is to it.
+
+    println("final result: $updatedTask")
+}
+
+/**
+ * Task Model
+ */
+data class Task(
+    val id: String = UUID.randomUUID().toString(),
+    val lastUpdate: Long = -1,
+    val title: String = "New Task",
+    val description: String = "",
+    val completed: Boolean = false
+)
+
+/**
+ * Used with the filter selector in the tasks list.
+ */
+enum class FilterType {
+    ANY,
+    ACTIVE,
+    COMPLETE ;
+
+    fun filter(task:Task):Boolean {
+        return when(this) {
+            ANY -> true
+            ACTIVE -> !task.completed
+            COMPLETE -> task.completed
+        }
+    }
+}
+
+/**
+ * Tasks Sync State
+ *
+ * Represents the app's current synchronization state.
+ */
+sealed class SyncState {
+    object IDLE : SyncState() {
+        override fun toString(): String = "IDLE"
+    }
+
+    data class PROCESS(val type: Type) : SyncState() {
+        enum class Type { REFRESH, CREATE, UPDATE }
+    }
+
+    data class ERROR(val throwable: Throwable) : SyncState()
+}
+
+data class TasksState(
+    val tasks: List<Task>,
+    val filter: FilterType,
+    val syncState: SyncState
+)
+
+/*
+    State Diagram for SyncState
+
+    @startuml
+    [*] --> IDLE
+    IDLE --> PROCESS : refresh
+
+    PROCESS --> IDLE : success
+    PROCESS --> ERROR : failed
+
+    ERROR --> IDLE : reset
+    ERROR: throwable
+    @enduml
+
+    Class Diagram for Tasks
+
+    @startuml
+    hide empty members
+    class Task {
+        id: String
+        lastUpdate: Long
+        title: String
+        description: String
+        completed: String
+    }
+    class TasksModelState <<(D,orchid) data class>>{
+        tasks:List<Task>,
+        filter: FilterType,
+        syncState: SyncState
+    }
+    enum SyncState <<sealed class>> {
+        Idle(),
+        Process(type),
+        Error(throwable)
+    }
+    enum FilterType {
+        ANY
+        ACTIVE
+        COMPLETE
+    }
+
+    TasksModelState*--Task:tasks
+    TasksModelState*--FilterType:filter
+    TasksModelState*--SyncState:syncState
+    @enduml
+ */
