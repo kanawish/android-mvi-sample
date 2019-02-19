88c82fb77ef21758ef92d23bfbee32b4a2e6c0bd
Etienne Caron <etienne.caron@shopify.com>
2019-02-19 14:05:03 -0500

    **Lesson: Setting up an Android MVI project**
    We've covered the basic theory behind MVI. We now want to apply it in practice. In this lesson, let's set up our Android MVI project. We'll sketch the overall structure of our app. Then we'll go over what libraries we will be using, and why.

--- app/src/main/java/com/kanawish/sample/mvi/view/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/kanawish/sample/mvi/view/statistics/StatisticsFragment.kt
@@ -1,5 +1,19 @@
 package com.kanawish.sample.mvi.view.statistics
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kanawish.sample.mvi.R
 
-class StatisticsFragment : Fragment()
+/**
+ * Shows statistics for the app.
+ */
+class StatisticsFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.statistics_frag, container, false)
+    }
+
+}
--- app/src/main/java/com/kanawish/sample/mvi/view/tasks/TasksActivity.kt
+++ app/src/main/java/com/kanawish/sample/mvi/view/tasks/TasksActivity.kt
@@ -1,5 +1,52 @@
 package com.kanawish.sample.mvi.view.tasks
 
+import android.os.Bundle
+import android.support.v4.view.GravityCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.MenuItem
+import com.kanawish.sample.mvi.R
+import com.kanawish.sample.mvi.util.replaceFragmentInActivity
+import com.kanawish.sample.mvi.util.setupActionBar
+import kotlinx.android.synthetic.main.tasks_act.drawerLayout
 
-class TasksActivity : AppCompatActivity()
+/**
+ * Tasks Activity houses the Toolbar, the nav UI, the FAB and the fragment holding the tasks list.
+ */
+class TasksActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tasks_act)
+
+        // Set up the toolbar.
+        setupActionBar(R.id.toolbar) {
+            setHomeAsUpIndicator(R.drawable.ic_menu)
+            setDisplayHomeAsUpEnabled(true)
+        }
+
+        // Set up the navigation drawer.
+        drawerLayout.apply {
+            setStatusBarBackground(R.color.colorPrimaryDark)
+        }
+
+        // Use existing content fragment, or create one from scratch.
+        supportFragmentManager.findFragmentById(R.id.contentFrame) as TasksFragment?
+            ?: TasksFragment().also {
+                replaceFragmentInActivity(it, R.id.contentFrame)
+            }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                // Open the navigation drawer when the home icon is selected from the toolbar.
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+}
