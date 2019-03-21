8b493f5a13f2d3ab0830b02c585647e3de7ae609
Etienne Caron <etienne.caron@shopify.com>
2019-03-21 15:29:45 -0400

    **Lesson: Completing `TasksFragment` and `AddEditTaskFragment`** In this lesson we add the missing _save task_ and _delete task_ intents in `AddEditTaskIntentFactory`, and add the _edit task_ and _complete task_ intents in the tasks list.

--- app/src/main/java/com/kanawish/sample/mvi/intent/AddEditTaskIntentFactory.kt
+++ app/src/main/java/com/kanawish/sample/mvi/intent/AddEditTaskIntentFactory.kt
@@ -5,6 +5,7 @@ import com.kanawish.sample.mvi.model.TaskEditorModelStore
 import com.kanawish.sample.mvi.model.TaskEditorState
 import com.kanawish.sample.mvi.model.TaskEditorState.Closed
 import com.kanawish.sample.mvi.model.TaskEditorState.Editing
+import com.kanawish.sample.mvi.model.TasksModelStore
 import com.kanawish.sample.mvi.view.addedittask.AddEditTaskViewEvent
 import com.kanawish.sample.mvi.view.addedittask.AddEditTaskViewEvent.CancelTaskClick
 import com.kanawish.sample.mvi.view.addedittask.AddEditTaskViewEvent.DeleteTaskClick
@@ -20,7 +21,8 @@ import javax.inject.Singleton
  * ModelStores, Repositories or Services.
  */
 @Singleton class AddEditTaskIntentFactory @Inject constructor(
-    private val taskEditorModelStore: TaskEditorModelStore
+    private val taskEditorModelStore: TaskEditorModelStore,
+    private val tasksModelStore: TasksModelStore
 ) {
 
     fun process(viewEvent: AddEditTaskViewEvent) {
@@ -31,9 +33,31 @@ import javax.inject.Singleton
         return when (viewEvent) {
             is TitleChange -> buildEditTitleIntent(viewEvent)
             is DescriptionChange -> buildEditDescriptionIntent(viewEvent)
-            SaveTaskClick -> TODO()
-            DeleteTaskClick -> TODO()
+            SaveTaskClick -> buildSaveIntent()
+            DeleteTaskClick -> buildDeleteIntent()
             CancelTaskClick -> buildCancelIntent()
+        }
+    }
+
+    /**
+     * An example of delegating work to an external dependency.
+     */
+    private fun buildSaveIntent() = editorIntent<Editing> {
+        // This triggers a state change in another ModelStore.
+        save().run {
+            // NOTE: When we do this with a real backend + retrofit, it will become asynchronous.
+            val intent = TasksIntentFactory.buildAddOrUpdateTaskIntent(task)
+            tasksModelStore.process(intent)
+            saved()
+        }
+    }
+
+    private fun buildDeleteIntent() = editorIntent<Editing> {
+        delete().run {
+            // `TasksStore` deletes this task from its internal list.
+            val intent = TasksIntentFactory.buildDeleteTaskIntent(taskId)
+            tasksModelStore.process(intent)
+            deleted()
         }
     }
 
@@ -53,6 +77,8 @@ import javax.inject.Singleton
         }
 
         fun buildAddTaskIntent(task: Task) = editorIntent<Closed> { addTask(task) }
+
+        fun buildEditTaskIntent(task: Task) = editorIntent<Closed> { editTask(task) }
 
         private fun buildEditTitleIntent(viewEvent: TitleChange) = editorIntent<Editing> {
             edit { copy( title = viewEvent.title ) }
--- app/src/main/java/com/kanawish/sample/mvi/intent/TasksIntentFactory.kt
+++ app/src/main/java/com/kanawish/sample/mvi/intent/TasksIntentFactory.kt
@@ -5,7 +5,13 @@ import com.kanawish.sample.mvi.model.TaskEditorModelStore
 import com.kanawish.sample.mvi.model.TasksModelStore
 import com.kanawish.sample.mvi.model.TasksState
 import com.kanawish.sample.mvi.view.tasks.TasksViewEvent
-import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.*
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.ClearCompletedClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.CompleteTaskClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.EditTaskClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.FilterTypeClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.NewTaskClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.RefreshTasksClick
+import com.kanawish.sample.mvi.view.tasks.TasksViewEvent.RefreshTasksSwipe
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -22,16 +28,58 @@ class TasksIntentFactory @Inject constructor(
         return when(viewEvent) {
             ClearCompletedClick -> TODO()
             FilterTypeClick -> TODO()
+            RefreshTasksSwipe, RefreshTasksClick -> TODO()
             NewTaskClick -> buildNewTaskIntent()
-            RefreshTasksClick -> TODO()
-            RefreshTasksSwipe -> TODO()
-            is CompleteTaskClick -> TODO()
-            is EditTaskClick -> TODO()
+            is CompleteTaskClick -> buildCompleteTaskClick(viewEvent)
+            is EditTaskClick -> buildEditTaskIntent(viewEvent)
+        }
+    }
+
+    private fun buildEditTaskIntent(viewEvent: TasksViewEvent.EditTaskClick): Intent<TasksState> {
+        // We use `sideEffect{}` here since we're entirely delegating the work.
+        return sideEffect {
+            // We can assert things about the TasksStore state.
+            assert(tasks.contains(viewEvent.task))
+
+            // Editing a task then only involves opening it.
+            val intent = AddEditTaskIntentFactory.buildEditTaskIntent(viewEvent.task)
+            taskEditorModelStore.process(intent)
         }
     }
 
     private fun buildNewTaskIntent(): Intent<TasksState> = sideEffect {
         val addIntent = AddEditTaskIntentFactory.buildAddTaskIntent(Task())
         taskEditorModelStore.process(addIntent)
+    }
+
+    private fun buildCompleteTaskClick(viewEvent: TasksViewEvent.CompleteTaskClick): Intent<TasksState> {
+        return intent {
+            // We need to operate on the tasks list here.
+            val mutableList = tasks.toMutableList()
+            // Replaces old task in the list with a new updated copy.
+            mutableList[tasks.indexOf(viewEvent.task)] =
+                viewEvent.task.copy(completed = viewEvent.checked)
+            // Take the modified list, and create a new copy of tasksState with it.
+            copy(tasks = mutableList)
+        }
+    }
+
+    companion object {
+        /** Allows an external Model to save a task. */
+        fun buildAddOrUpdateTaskIntent(task:Task) : Intent<TasksState> = intent {
+            tasks.toMutableList().let { newList ->
+                newList.find { task.id == it.id }?.let {
+                    newList[newList.indexOf(it)] = task
+                } ?: newList.add(task)
+                copy(tasks = newList)
+            }
+        }
+
+        /** Allows an external model to delete a task. */
+        fun buildDeleteTaskIntent(taskId:String): Intent<TasksState> = intent {
+            copy( tasks = tasks.toMutableList().apply {
+                find { it.id == taskId }?.also { remove(it) }
+            })
+        }
     }
 }
